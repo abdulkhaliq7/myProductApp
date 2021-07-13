4315c710b8956a86d1bf38073f5acce2ba1610fd
abdulkhaliq7 <abdikhaliq_7@hotmail.com>
2021-07-13 06:24:22 +0300

    added expo navigator

---
+++ firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDAVj-PDlicdnVGSukU_xcRjsS9BhG1eAw",
+    authDomain: "myproductapp-3d579.firebaseapp.com",
+    projectId: "myproductapp-3d579",
+    storageBucket: "myproductapp-3d579.appspot.com",
+    messagingSenderId: "883863217151",
+    appId: "1:883863217151:web:971a6a792f9595150b3dca"
+  };
