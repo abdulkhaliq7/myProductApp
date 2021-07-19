fb34407d333b1a9ce45e81226fa65e2ad2118cd4
abdulkhaliq7 <abdikhaliq_7@hotmail.com>
2021-07-19 10:00:40 +0300

    added dashboard total price and product

--- screens/dashboardScreen.js
+++ screens/dashboardScreen.js
@@ -11,6 +11,43 @@ const HomeScreen = ( { navigation }) => {
 
     const [products, setProducts] = useState([])
 
+    const [totalProduct, setTotalProduct] = useState([])
+
+    const [totalPrice, setTotalPrice] = useState('')
+
+    useEffect(() => {
+
+        const totalPrices = totalProduct.reduce((total, price) => {
+            return total + price.data
+        }, 0)
+        setTotalPrice(totalPrices)
+        console.log(totalPrice)
+    }, [totalProduct])
+    
+
+    
+
+
+    useEffect(() => {
+
+        const getProduct = async () => {
+            await db.collection('users')
+                    .doc(auth?.currentUser?.uid)
+                    .collection('products')
+                    .get()
+                    .then((querySnapshot) => {
+                        setTotalProduct(querySnapshot.docs.map((doc) => ({
+                            // doc.data() is never undefined for query doc snapshots
+                            // console.log(doc.id, " => ", doc.data());
+                            data: doc.data().productPrice
+                    })))
+            });
+        }
+    getProduct()
+
+    
+    }, [])
+    
 
     useEffect(() => {
         if(auth?.currentUser) {
@@ -93,6 +130,12 @@ const HomeScreen = ( { navigation }) => {
         <SafeAreaView>
             <Text h4 style={{ marginBottom: 10, marginLeft: 20}}>
             Top Five most Expensive Products
+            </Text>
+            <Text h4 style={{ marginBottom: 10, marginLeft: 20}}>
+            Total Products: {totalProduct.length}
+            </Text>
+            <Text h4 style={{ marginBottom: 10, marginLeft: 20}}>
+            Total Price: $ {totalPrice}
             </Text>
             <ScrollView style={styles.container}>
                 {products.map(({id, data: { productName, productDescription, productPrice, productImageUrl }}) => (
